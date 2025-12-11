3ac2d4adb7a0a6291154cfe7b7e4a690980ef26f
Amy Gonzalez <amaice8@gmail.com>
2025-12-10 18:34:00 -0600

    day 3

--- 3/main.cpp
+++ 3/main.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 
 int largest_joltage(const std::string&);
+int choi(char);
 
 int main() {
-    const std::string filePath = "input";
+    const std::string filePath = "input_small";
     std::ifstream inputFile(filePath);
 
     std::string line;
@@ -12,22 +13,28 @@ int main() {
     while (getline(inputFile, line)) {
         max_joltage += largest_joltage(line);
     }
-    std::cout << "Largest joltage: " << largest_joltage;
+    std::cout << "Largest joltage: " << max_joltage << std::endl;
     return 0;
 }
 
 int largest_joltage(const std::string& bank) {
-    int battery_one_index = bank[0] - '0';
-    int battery_two_index = bank[1] - '0';
-    for (int i = 0; i < bank.length(); i++) {
-        if ((bank[i] - '0') > bank[battery_one_index]) {
+    int battery_one_index = choi(bank[0]);
+    int battery_two_index = choi(bank[1]);
+    for (int i = 0; i < bank.length() - 1; i++) {
+        if (choi(bank[i]) > bank[battery_one_index]) {
             battery_one_index = i;
         }
     }
 
-    for (int i = 0; i < bank.length(); i++) {
-        
+    for (int i = battery_one_index + 1; i < bank.length(); i++) {
+        if (choi(bank[i]) > bank[battery_two_index]) {
+            battery_two_index = i;
+        }
     }
 
-    return 0;
+    return choi(bank[battery_one_index]) * 10 + choi(bank[battery_two_index]);
+}
+
+int choi(const char c) {
+    return c - '0';
 }
